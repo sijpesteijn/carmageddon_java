335a362697773bb305be79888e953b1ffe023337
gijs.sijpesteijn <gijs@jpoint.nl>
2016-06-08 21:59:51 +0200

    Inital commit

---
+++ program/src/carmageddon.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : carmageddon.cpp
+// Author      : Carmageddon
+// Version     :
+// Copyright   : Nothing much
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Carmageddon on a roll." << endl; // prints Carmageddon on a roll.
+	return 0;
+}
