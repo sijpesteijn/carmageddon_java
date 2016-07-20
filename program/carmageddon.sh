efecda5d3a2def9295937a166a662435d26adfc0
Gijs Sijpesteijn <gijs@jpoint.nl>
2016-07-20 16:16:16 -0400

    Update carmageddon.sh

--- program/carmageddon.sh
+++ program/carmageddon.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-mount /dev/mmcblk0p1 /media
+mount /dev/mmcblk0p1 /mnt
 export SLOTS=/sys/devices/bone_capemgr.9/slots
 
 echo 'am33xx_pwm' > $SLOTS
 echo 'bone_pwm_P9_22' > $SLOTS
 echo 'bone_pwm_P9_42' > $SLOTS
 echo 'BB-UART4' > $SLOTS
+
+stty -F /dev/ttyO4 115200 raw -echo
