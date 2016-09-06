e60056afce432cb79c0c27ee8818b21ec4560554
gijs.sijpesteijn <gijs@jpoint.nl>
2016-09-06 16:12:50 +0200

    Klein fimpje dat je kan streamen.

---
+++ beaglebone/carmageddon.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+ifdown wlan0
+ifup wlan0
+
+export JAVA_HOME=/usr/local/java
+export PATH=$PATH:$JAVA_HOME/bin
+export SLOTS=/sys/devices/bone_capemgr.9/slots
+
+mount /dev/mmcblk0 /mnt/media/
+
+echo 'am33xx_pwm' > $SLOTS
+echo 'bone_pwm_P9_22' > $SLOTS
+echo 'bone_pwm_P9_42' > $SLOTS
--- src/main/java/nl/carmageddon/WebCamController.java
+++ src/main/java/nl/carmageddon/WebCamController.java
@@ -14,12 +14,14 @@ import javax.inject.Singleton;
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.*;
-import javax.ws.rs.core.Context;
-import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.*;
 import java.awt.image.BufferedImage;
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
+import java.net.URL;
+import java.nio.channels.Channels;
+import java.nio.channels.FileChannel;
+import java.nio.channels.WritableByteChannel;
+import java.util.Date;
 
 /**
  * @author Gijs Sijpesteijn
@@ -29,14 +31,20 @@ import java.io.IOException;
 public class WebCamController {
     private static Logger logger = LoggerFactory.getLogger(CarController.class);
     private VideoCapture camera;
+    private File video;
+    final int chunk_size = 1024 * 1024; // 1MB chunks
 
     @Inject
     public WebCamController(Configuration configuration) throws IOException {
-        String libPath = System.getProperty("java.library.path");
-        String opencvPath = "./main/resources/";
-        System.setProperty("java.library.path", libPath + ":" + opencvPath);
+        if (!System.getProperty("os.arch").contains("arm")) {
+            String libPath = System.getProperty("java.library.path");
+            String opencvPath = "./main/resources/";
+            System.setProperty("java.library.path", libPath + ":" + opencvPath);
+        }
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
         openCamera(0);
+        URL url = this.getClass().getResource("/output.mp4");
+        video = new File(url.getFile());
     }
 
     @POST
@@ -57,17 +65,23 @@ public class WebCamController {
         return true;
     }
 
+    @POST
+    @Path(value = "/capture")
+    public void startCapture() {
+
+    }
+
     @GET
     @Path(value = "/snapshot")
     @Produces(MediaType.TEXT_HTML)
     public void makeSnapShot(@Context HttpServletResponse response) throws IOException {
         Mat frame = new Mat();
         camera.read(frame);
-        Imgcodecs.imwrite(System.getProperty("java.io.tmpdir") + "/snapshot.png", frame);
+        Imgcodecs.imwrite(System.getProperty("java.io.tmpdir") + "/snapshot.jpeg", frame);
 
         ByteArrayOutputStream jpegOutputStream = new ByteArrayOutputStream();
 
-        BufferedImage image = ImageIO.read(new File(System.getProperty("java.io.tmpdir") + "/snapshot.png"));
+        BufferedImage image = ImageIO.read(new File(System.getProperty("java.io.tmpdir") + "/snapshot.jpeg"));
         ImageIO.write(image, "jpeg", jpegOutputStream);
 
         byte[] imgByte = jpegOutputStream.toByteArray();
@@ -82,10 +96,84 @@ public class WebCamController {
         responseOutputStream.close();
     }
 
+    @GET
+    @Path(value = "/stream")
+    @Produces("video/mp4")
+    public Response stream(@HeaderParam("range") String range) throws IOException {
+        if (range == null) {
+            StreamingOutput streamer = output -> {
+
+                final FileChannel inputChannel = new FileInputStream(video).getChannel();
+                final WritableByteChannel outputChannel = Channels.newChannel(output);
+                try {
+                    inputChannel.transferTo(0, inputChannel.size(), outputChannel);
+                } finally {
+                    // closing the channels
+                    inputChannel.close();
+                    outputChannel.close();
+                }
+            };
+            return Response.ok(streamer).status(200).header(HttpHeaders.CONTENT_LENGTH, video.length()).build();
+        }
+        String[] ranges = range.split("-");
+        final int from = Integer.parseInt(ranges[0]);
+        /**
+         * Chunk media if the range upper bound is unspecified. Chrome sends "bytes=0-"
+         */
+        int to = chunk_size + from;
+        if (to >= video.length()) {
+            to = (int) (video.length() - 1);
+        }
+        if (ranges.length == 2) {
+            to = Integer.parseInt(ranges[1]);
+        }
+
+        final String responseRange = String.format("bytes %d-%d/%d", from, to, video.length());
+        final RandomAccessFile raf = new RandomAccessFile(video, "r");
+        raf.seek(from);
+
+        final int len = to - from + 1;
+        final MediaStreamer streamer = new MediaStreamer(len, raf);
+        Response.ResponseBuilder res = Response.ok(streamer).status(206)
+                .header("Accept-Ranges", "bytes")
+                .header("Content-Range", responseRange)
+                .header(HttpHeaders.CONTENT_LENGTH, streamer.getLenth())
+                .header(HttpHeaders.LAST_MODIFIED, new Date(video.lastModified()));
+        return res.build();
+    }
+
     @PUT
     @Path(value = "/data/{data}")
     public void setData(@PathParam("data") String data) {
         System.out.println("put data: " + data);
     }
 
+    public class MediaStreamer implements StreamingOutput {
+
+        private int length;
+        private RandomAccessFile raf;
+        final byte[] buf = new byte[4096];
+
+        public MediaStreamer(int length, RandomAccessFile raf) {
+            this.length = length;
+            this.raf = raf;
+        }
+
+        @Override
+        public void write(OutputStream outputStream) throws IOException, WebApplicationException {
+            try {
+                while( length != 0) {
+                    int read = raf.read(buf, 0, buf.length > length ? length : buf.length);
+                    outputStream.write(buf, 0, read);
+                    length -= read;
+                }
+            } finally {
+                raf.close();
+            }
+        }
+
+        public int getLenth() {
+            return length;
+        }
+    }
 }
